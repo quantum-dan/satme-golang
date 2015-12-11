151e4419a14ef9ee8ce78c84503dcb41b86c491f
Daniel Philippus <dphilippus123@gmail.com>
2015-12-11 17:15:56 +0000

    Forms handling

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ with Goroutines (green threads).  This may or may not be the production version.
 import (
 	// "github.com/gorilla/sessions"
 	"github.com/gorilla/mux"
-	// "github.com/gorilla/schema"
+	"github.com/gorilla/schema"
 	"net/http"
 	"html/template"
 	"log"
@@ -25,12 +25,19 @@ type Person struct {
 	Name string
 }
 
+type NameForm struct {
+	Get bool `schema:"-"`
+	Name string `schema:"name"`
+}
+
 func main() {
 	var PORT int = 8080
 	r := mux.NewRouter()
 	r.HandleFunc("/", index)
 	r.HandleFunc("/static/{file}", serve_static)
 	r.HandleFunc("/tmpl", tmpl_demo)
+	r.HandleFunc("/form_demo", get_form_demo)
+	r.HandleFunc("/form", form_demo)
 	http.Handle("/", r)
 	logstr := fmt.Sprintf("Listening on port %d", PORT)
 	log.Println(logstr)
@@ -40,6 +47,40 @@ func main() {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
+
+func form_demo(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "Error 500: internal server error.  Failed to parse form.")
+		log.Println("Error parsing form in form_demo")
+	} else {
+		decoder := schema.NewDecoder()
+		results := new(NameForm)
+		err = decoder.Decode(results, r.PostForm)
+		if err != nil {
+			fmt.Fprintf(w, "Error 500: internal server error.  Failed to evaluate form.")
+			log.Println("Error reading form in form_demo")
+		} else {
+			t, _ := template.ParseFiles("templates/form.html")
+			results.Get = false
+			err = t.Execute(w, results)
+			if err != nil {
+				fmt.Fprintf(w, "Error 500: internal server error.  Failed to execute template.")
+				log.Println("Failed to execute template in form_demo")
+			}
+		}
+	}
+}
+
+func get_form_demo(w http.ResponseWriter, r *http.Request) {
+	val := NameForm{Get: true, Name: ""}
+	t, _ := template.ParseFiles("templates/form.html")
+	err := t.Execute(w, val)
+	if err != nil {
+		fmt.Fprintf(w, "Error 500: internal server error.  Failed to execute template.")
+		log.Println("Failed to execute template in get_form_demo")
+	}
+} 
 
 func serve_static(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "static/" + mux.Vars(r)["file"])
